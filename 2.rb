489ebbf1f7e4e4a7481451f89e94e63e377b197e
Kay Mok <kaymok3@gmail.com>
2018-06-10 17:07:56 -0400

    Fix edge case

--- 2.rb
+++ 2.rb
@@ -7,15 +7,26 @@
 # For s = "2[b3[a]]", the output should be decodeString(s) = "baaabaaa"
 
 def decode_string(s)
-  # start from inside to outside, replace k[encoded] with encoded * k
-  re = /(\d+)\[(.*)\]/
-  while s.match(re)[2].match(re)
-    decoded_inner = s.match(re)[2].sub(s.match(re)[2].match(re)[0], s.match(re)[2].match(re)[2] * s.match(re)[2].match(re)[1].to_i)
-    s = s.sub(s.match(re)[2], decoded_inner)
+  # check that square brackets are not nested
+  if s.rindex('[') > s.index(']')
+    non_nested_re = /(\d+)\[(.*?)\]/
+    # for each ocurrence of k[encoded], replace it with encoded * k
+    s.scan(non_nested_re).each do |x|
+      s = s.sub("#{x[0]}[#{x[1]}]", x[1] * x[0].to_i)
+    end
+  else
+  # if nested, work from inside to outside to replace k[encoded] with encoded * k
+    re = /(\d+)\[(.*)\]/
+    while s.match(re)[2].match(re)
+      decoded_inner = s.match(re)[2].sub(s.match(re)[2].match(re)[0], s.match(re)[2].match(re)[2] * s.match(re)[2].match(re)[1].to_i)
+      s = s.sub(s.match(re)[2], decoded_inner)
+    end
+    s = s.sub(s.match(re)[0], s.match(re)[2] * s.match(re)[1].to_i)
   end
-  s.sub(s.match(re)[0], s.match(re)[2] * s.match(re)[1].to_i)
+  s
 end
 
 puts decode_string("b3[a]")
 puts decode_string("4[ab]")
 puts decode_string("2[b3[a]]")
+puts decode_string("2[abc]3[cd]ef")
