a34cfe6800f2b2aee5708c0686da89ec5087c7bd
Kay Mok <kaymok3@gmail.com>
2018-06-14 13:52:40 -0400

    Improve solution

--- 2.rb
+++ 2.rb
@@ -6,22 +6,28 @@
 # For s = "4[ab]", the output should be decodeString(s) = "abababab"
 # For s = "2[b3[a]]", the output should be decodeString(s) = "baaabaaa"
 
+# Note: this solution only works with 2 levels of nested square brackets
+
 def decode_string(s)
-  # check that square brackets are not nested
-  if s.rindex('[') > s.index(']')
-    non_nested_re = /(\d+)\[(.*?)\]/
-    # for each ocurrence of k[encoded], replace it with encoded * k
-    s.scan(non_nested_re).each do |x|
-      s = s.sub("#{x[0]}[#{x[1]}]", x[1] * x[0].to_i)
+  # regex to catch multiple encoded
+  mult_re = /(\d+)\[(.*?)\]/
+  # regex to catch single encoded
+  re = /(\d+)\[(.*)\]/
+  substring = s.match(re)[2]
+  # check that square brackets are nested and replace k[encoded] with encoded * k in substring
+  if substring.scan(mult_re).length > 1
+    substring.scan(mult_re).each do |x|
+      substring = substring.sub("#{x[0]}[#{x[1]}]", x[1] * x[0].to_i)
     end
-  else
-  # if nested, work from inside to outside to replace k[encoded] with encoded * k
-    re = /(\d+)\[(.*)\]/
-    while s.match(re)[2].match(re)
-      decoded_inner = s.match(re)[2].sub(s.match(re)[2].match(re)[0], s.match(re)[2].match(re)[2] * s.match(re)[2].match(re)[1].to_i)
-      s = s.sub(s.match(re)[2], decoded_inner)
+  elsif substring.scan(re).length == 1
+    substring.scan(re).each do |x|
+      substring = substring.sub("#{x[0]}[#{x[1]}]", x[1] * x[0].to_i)
     end
-    s = s.sub(s.match(re)[0], s.match(re)[2] * s.match(re)[1].to_i)
+  end
+  # for each ocurrence of k[encoded], replace it with encoded * k
+  s = s.sub(s.match(re)[2], substring)
+  s.scan(mult_re).each do |x|
+    s = s.sub("#{x[0]}[#{x[1]}]", x[1] * x[0].to_i)
   end
   s
 end
@@ -30,3 +36,5 @@ puts decode_string("b3[a]")
 puts decode_string("4[ab]")
 puts decode_string("2[b3[a]]")
 puts decode_string("2[abc]3[cd]ef")
+puts decode_string("10[ab2[cde]ed]")
+puts decode_string("3[hi2[kay]1[yo]bye]")
